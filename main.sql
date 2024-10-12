5d4eef4354e3d3945096f1fd327fdf79bdd6e085
houseisround <106474124+houseisround@users.noreply.github.com>
2024-10-12 10:34:07 +0400

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,12 +1,36 @@
 select model, speed, hd
 from PC
-where price < 500
+where price < 500;
 
 select distinct maker
 from Product
-where type = 'Printer'
+where type = 'Printer';
 
 select model, ram, screen
 from Laptop
-where price > 1000
+where price > 1000;
+
+select *
+from Printer 
+where color = 'y';
+
+select model, speed, hd
+from PC
+where cd in ('12x', '24x') and price < 600;
+
+select distinct p.maker, l.speed
+from Product p join Laptop l on p.model = l.model
+where hd >= 10;
+
+select p.model, pc.price
+from Product p join PC pc on p.model = pc.model
+where p.maker = 'B'
+UNION
+select p.model, l.price
+from Product p join Laptop l on p.model = l.model
+where p.maker = 'B'
+UNION
+select p.model, pr.price
+from Product p join Printer pr on p.model = pr.model
+where p.maker = 'B';
 
