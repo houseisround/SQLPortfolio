8905ffb313535080cea68000b800d0b6d69257a8
houseisround <106474124+houseisround@users.noreply.github.com>
2025-01-03 16:53:15 +0400

    Update sql_practice.sql

--- sql_practice.sql
+++ sql_practice.sql
@@ -12,3 +12,14 @@ SELECT courier_id,
 FROM   courier_actions
 ORDER BY courier_id, action, time desc limit 1000;
 
+SELECT name,
+       price
+FROM   products
+ORDER BY price desc 
+limit 5
+
+SELECT name product_name,
+       price product_price
+FROM   products
+ORDER BY price desc 
+limit 5;
